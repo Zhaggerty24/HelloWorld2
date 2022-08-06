d677dd4b88e1f40b1d7e03e4f0e358894cc40e25
Zachary Haggerty <zhaggerty24@gmail.com>
2022-08-06 11:34:29 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorld2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HelloWorld");
+        }
+    }
+}
